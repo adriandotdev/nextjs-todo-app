76649db2c40be1050c1c58c310d271f6749b4993
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-10-13 09:37:01 +0800

    development: CHANGED the position property value of ConfirmationModal
    FROM absolute to fixed

--- components/ConfirmationModal.jsx
+++ components/ConfirmationModal.jsx
@@ -21,7 +21,7 @@ const ConfirmationModal = ({
 
 	return (
 		<div
-			className="absolute top-0 bottom-0 left-0 right-0 bg-black bg-opacity-25 flex justify-center items-end z-10 lg:items-center"
+			className="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-25 flex justify-center items-end z-10 lg:items-center"
 			onClick={() =>
 				setConfirmationModal({
 					...confirmationModal,
