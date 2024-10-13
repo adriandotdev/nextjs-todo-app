dc81776c95085d07a19beadeb1c93c169d50e84e
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-10-13 21:23:40 +0800

    development: ADDED account's name in navigation

--- components/TodoNavigation.jsx
+++ components/TodoNavigation.jsx
@@ -27,31 +27,35 @@ const TodoNavigation = () => {
 
 	return (
 		<>
-			<ul className="text-white flex gap-5 px-2 py-5 justify-end items-center lg:px-10">
-				<li>
-					<Link href="/todo/list">Todo</Link>
-				</li>
-				<li>
-					<Link href="/notes">Notes</Link>
-				</li>
-				<li>
-					<IconButton
-						aria-label="logout"
-						color="error"
-						onClick={() =>
-							setConfirmationModal({
-								...confirmationModal,
-								is_visible: true,
-								confirmation_message: "Are you sure you want to sign out?",
-								button_confirmation_text: "Sign Out",
-								event: Logout,
-							})
-						}
-					>
-						<LogoutIcon />
-					</IconButton>
-				</li>
-			</ul>
+			<div className="flex justify-between items-center px-5 py-3">
+				<h1 className="font-bold text-orange-300">User</h1>
+				<ul className="text-white flex gap-5 justify-end items-center">
+					<li>
+						<Link href="/todo/list">Todo</Link>
+					</li>
+					<li>
+						<Link href="/notes">Notes</Link>
+					</li>
+					<li>
+						<IconButton
+							aria-label="logout"
+							color="error"
+							onClick={() =>
+								setConfirmationModal({
+									...confirmationModal,
+									is_visible: true,
+									confirmation_message: "Are you sure you want to sign out?",
+									button_confirmation_text: "Sign Out",
+									event: Logout,
+								})
+							}
+						>
+							<LogoutIcon />
+						</IconButton>
+					</li>
+				</ul>
+			</div>
+
 			{confirmationModal.is_visible && (
 				<ConfirmationModal
 					confirmationModal={confirmationModal}
