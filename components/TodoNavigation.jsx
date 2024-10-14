1ec333ecc7fae9e335d8b13f508c98b81693ebc2
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-10-14 12:11:43 +0800

    development: INTEGRATED API for getting user details
    The component that using that API is TodoNavigation which gets the name
    of the user and displays at the left side of the navigation bar.

--- components/TodoNavigation.jsx
+++ components/TodoNavigation.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Link from "next/link";
 import { IconButton } from "@mui/material";
@@ -11,6 +11,8 @@ import axios from "axios";
 
 const TodoNavigation = () => {
 	const router = useRouter();
+
+	const [user, setUser] = useState(() => ({}));
 
 	const [confirmationModal, setConfirmationModal] = useState({
 		is_visible: false,
@@ -25,10 +27,76 @@ const TodoNavigation = () => {
 		router.push("/signin");
 	};
 
+	const apiClient = axios.create({
+		baseURL: process.env.BASE_URL,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	useEffect(() => {
+		let interceptorID;
+
+		async function GetUserDetails() {
+			interceptorID = apiClient.interceptors.response.use(
+				(response) => {
+					return response;
+				},
+				async (error) => {
+					const originalRequest = error.config;
+
+					if (
+						error.response &&
+						error.response.status === 401 &&
+						!originalRequest._retry
+					) {
+						originalRequest._retry = true;
+
+						try {
+							const result = await axios.get("/api/refresh");
+
+							if (result.status === 200)
+								// Retry the original request with refreshed token
+								return apiClient(originalRequest);
+							else {
+								// If refresh fails, redirect to the sign-in page
+								await axios.get("/api/users/logout");
+								router.push("/signin");
+								return Promise.reject(new Error("Token refresh failed"));
+							}
+						} catch (refreshError) {
+							// In case of refresh failure, redirect to sign-in page and reject
+							await axios.get("/api/users/logout");
+							router.push("/signin");
+							return Promise.reject(refreshError);
+						}
+					}
+
+					// For other types of errors, reject the promise as usual
+					return Promise.reject(error);
+				}
+			);
+
+			try {
+				const result = await apiClient.get("/api/users/details");
+
+				console.log(result);
+				setUser(result.data.data);
+			} catch (err) {
+				console.error("Error fetching user details:", todosError);
+			}
+		}
+
+		GetUserDetails();
+
+		return () => {
+			apiClient.interceptors.response.eject(interceptorID);
+		};
+	}, []);
 	return (
 		<>
 			<div className="flex justify-between items-center px-5 py-3">
-				<h1 className="font-bold text-orange-300">User</h1>
+				<h1 className="font-bold text-orange-300">{user.name}</h1>
 				<ul className="text-white flex gap-5 justify-end items-center">
 					<li>
 						<Link href="/todo/list">Todo</Link>
