24f53c248d414c458664df7638d8450c1de55df3
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-10-26 21:15:30 +0800

    development: ADDED padding x in UpdateProfileForm component

--- app/todo/profile/page.jsx
+++ app/todo/profile/page.jsx
@@ -3,7 +3,7 @@ import UpdateProfileForm from "@components/UpdateProfileForm";
 const ProfilePage = () => {
 	return (
 		<>
-			<div className="flex flex-col items-center justify-center  w-full h-full pt-12 gap-3 pb-12">
+			<div className="flex flex-col items-center justify-center  w-full h-full pt-12 gap-3 pb-12 px-7">
 				<h1 className="text-3xl font-bold font-[Poppins] text-orange-500">
 					Profile
 				</h1>
